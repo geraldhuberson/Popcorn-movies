c197c9828d270278a5515502d0f7ab9cd55855d9
francoosman12 <francoosman12@gmail.com>
2021-12-13 18:59:27 -0600

    cambios en registro

--- js/registrarse.js
+++ js/registrarse.js
@@ -17,7 +17,7 @@ const campos = {
     validationServer02: false,
 	usuario: false,
     inputState: true,
-	validationServer06: false,
+	validationServer06: false, 
 	validationServer07:false,
 }
 
@@ -29,78 +29,122 @@ const validarFormulario = (e) => {
 		case "validationServer01":
 			validarCampo(expresiones.validationServer01, e.target, 'validationServer01');
 		break;
-		case "password":
+		case "validationServer06":
 			validarCampo(expresiones.validationServer06, e.target, 'validationServer06');
-			validationServer07();
 		break;
-		case "alidationServer07":
-			validationServer07();
+		case "validationServer07":
+			validarCampo(expresiones.validationServer07, e.target, 'validationServer07');
+			validarPassword2()
 		break;
 		case "validationServer02":
 			validarCampo(expresiones.validationServer02, e.target, 'validationServer02');
+		break;
+		case "inputState":
+			inputState();
 	}
 }
-
 const validarCampo = (expresion, input, campo) => {
-	if(expresion.test(input.value)){
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+	if(expresion.test(input.value)){		
+		document.getElementById(`${campo}`).classList.remove('is-invalid');
+		document.getElementById(`${campo}`).classList.add('is-valid');
+		// document.getElementById(`${campo}`).classList.add(' is-valid');
+		// document.getElementById(`${campo}`).classList.add(' is-invalid');
 		campos[campo] = true;
-	} else {
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+	}
+	else{
+		document.getElementById(`${campo}`).classList.add('is-invalid');
+		document.getElementById(`${campo}`).classList.remove('is-valid');
+		// document.getElementById(`${campo}`).classList.add(' is-valid');
+		// document.getElementById(`${campo}`).classList.add(' is-invalid');
 		campos[campo] = false;
 	}
+	console.log("test");
 }
+
+
+const validarPassword= (e) => {
+switch (e.target.name){
+	    case "validationServer06":
+			validarCampo(expresiones.validationServer06, e.target, 'validationServer06');
+			validarPassword2();
+		break;
+		case "validationServer07":
+			validarPassword2();
+		break;
+}
+}
+        
+//Ambas contraseñas deben coincidir:
 
 const validarPassword2 = () => {
-	const inputPassword1 = document.getElementById('password');
-	const inputPassword2 = document.getElementById('password2');
+	const inputvalidationServer06 = document.getElementById('validationServer06');
+	const inputvalidationServer07 = document.getElementById('validationServer07');
 
-	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos['password'] = false;
+	if(inputvalidationServer06.value !== inputvalidationServer07.value){
+		document.getElementById(`validationServer07`).classList.add('is-invalid');
+		document.getElementById(`validationServer07`).classList.remove('is-valid');
+		campos['validationServer06'] = false;
 	} else {
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos['password'] = true;
+		document.getElementById(`validationServer07`).classList.add('is-valid');
+		document.getElementById(`validationServer07`).classList.remove('is-invalid');
+		campos['validationServer06'] = true;
 	}
 }
+//tipeo
 
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario);
 	input.addEventListener('blur', validarFormulario);
 });
 
+//GUARDAR DATOS EN EL LOCALSTORAGE
+
+function guardarUsuario(){
+    const nombre=document.getElementById('validationServer01').value;
+    const apellido=document.getElementById('validationServer02').value;
+    const usuario=document.getElementById('usuario').value;
+    const pais=document.getElementById('inputState').value;
+    const contraseña=document.getElementById('validationServer06').value;
+    let usuarios= JSON.parse(localStorage.getItem('usuarios'))
+    if (usuarios==null){
+        usuarios=[]
+    }
+    const newUsuario={
+        id: usuarios.length+1,
+        nombre: nombre,
+        director: apellido,
+        genero: usuario,
+        categoria: pais,
+        año: contraseña,
+        
+    }
+ 
+    usuarios.push(newUsuario)
+    let usuarioLS=JSON.stringify(usuarios)
+    localStorage.setItem('usuarios',usuarioLS)
+    document.getElementById('formulario').reset()
+        
+    }
+
+//FIN DEL LOCALSTORAGE
+
 formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
+	if(campos.usuario && campos.validationServer01 && campos.validationServer02 && campos.validationServer06
+		 && campos.validationServer07 && campos.inputState && terminos.checked ){
 		formulario.reset();
 
-		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		document.getElementById('Formulario').classList.add('valid-feedback');
 		setTimeout(() => {
-			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+			document.getElementById('Formulario').classList.remove('');
 		}, 5000);
 
-		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
-			icono.classList.remove('formulario__grupo-correcto');
+		document.querySelectorAll('.valid-feedback').forEach((icono) => {
+			icono.classList.remove('');
 		});
 	} else {
-		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+		document.getElementById('checkFormulario').classList.add('');
 	}
 });
