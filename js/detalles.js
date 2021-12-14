f835210e17b915f8f721b02994b96625d2ce368f
Juan Ernesto Villegas <jevillegas_123@hotmail.com>
2021-12-13 23:01:47 -0300

    Agregados

---
+++ js/detalles.js
@@ -0,0 +1,73 @@
+class Pelicula {
+    constructor (titulo, imagen, logo, genero, año, duracion, elenco, sinopsis) {
+        this.titulo = titulo;
+        this.imagen = imagen;
+        this.logo = logo;
+        this.genero = genero;
+        this.año = año;
+        this.duracion = duracion;
+        this.elenco = elenco;
+        this.sinopsis = sinopsis;
+    }
+}
+
+let harryPotter = new Pelicula  (
+    titulo = "Harry Potter",
+    imagen = 
+    genero = " Fantasia",
+    año = " 2011",
+    duracion = " 2 hs 34 min",
+    elenco = " Daniel Raadcliffe, Rupert Grint, Emma Watson, Ralph Fiennes, Maggie Smith, Michael Gambon, Alan Rickman, Helena Bonham Carter, Robbie Coltrane.",
+    sinopsis = "El final ha llegado. Harry, Hermione y Ron tendrán que recuperar la espada de Gryffindor para encontrar y destruir los últimos horrocruxes. Mientras tanto, Lord Voldemort está a punto de apoderarse por completo de Hogwarts y de conseguir su objetivo: matar a Harry Potter. La única esperanza de Harry es encontrar los horrocruxes antes de que Voldemort lo encuentre a él. Buscando pistas, descubre una antigua y olvidada historia: la leyenda de las reliquias de la muerte, que podría dar al malvado Lord el poder definitivo. Pero el futuro de Harry está escrito desde que nació e incluye una misión para la que se ha estado preparando desde que llegó a Hogwarts: la batalla final contra Voldemort.",
+)
+
+let peliculaDetalles = document.getElementById ('contenido');
+
+peliculaDetalles.innerHTML = `
+    <div id="portadaPeli">
+        <img src="img/harrypotter.jpg" alt="portada de la pelicula" class="w-100" height="550px">
+    </div>
+    <div class="container">
+        <div class="encimaImagen">
+            <div>
+                <img src="img/logo-hp.png" alt="logo de la pelicula" width="25%" class="my-3">
+            </div>
+            <button type="button" class="btn btn-light me-4 my-4">
+                <i class="fas fa-play me-2"></i>
+                Reproducir
+            </button>
+            <button type="button" class="btn btn-dark rounded-circle me-3">
+                <i class="fas fa-plus"></i>
+            </button>
+            <button type="button" class="btn btn-dark rounded-circle me-3">
+                <i class="far fa-thumbs-up"></i>
+            </button>
+            <button type="button" class="btn btn-dark rounded-circle me-3">
+                <i class="far fa-thumbs-down"></i>
+            </button>
+        </div>
+        <div class="my-4 text-secondary">
+            <div class="row fs-5">
+                <div class="col-sm-12 col-md-6">
+                    <p><strong class="text-light">Genero:</strong>${this.genero}</p>
+                    <p><strong class="text-light">Año de salida:</strong>${this.año}</p>
+                    <p><strong class="text-light">Duracion:</strong>${this.duracion}</p>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <p><strong class="text-light">Elenco:</strong>${this.elenco}</p>
+                </div>
+            </div>
+            <div class="text-center my-4">
+                <h3>Ver mas</h3>
+                <a><i class="fas fa-arrow-down" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample"></i></a>
+                <div class="collapse" id="collapseExample">
+            <div class="card card-body bg-transparent">
+                <h5>${this.sinopsis}</h5>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+`
+
+    
---
+++ js/login.js
@@ -0,0 +1,44 @@
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}   
+
+class User {
+	constructor(name, lastname, email, password, admin) {
+	this.name = name;
+	this.lastname = lastname;
+	this.email = email;
+	this.password = password;
+	this.admin = admin;
+	}
+}
+
+let users = [
+	new User('juan','portillo','juanpablo.portillo2@gmail.com','12345678', false)
+	
+]
+
+
+
+function loginCheck(event) {
+	event.preventDefault();
+	let email = document.querySelector('#email').value;
+	let pass = document.querySelector('#pass').value;
+	let userLogged = users.find(User=>User.email == email);
+	if(userLogged && userLogged.password == pass){
+		window.location.assign('http://www.google.com.ar');
+	}else {
+		let dataError = document.createElement('div');
+		dataError.innerText='Los datos ingresados no son correctos';
+		dataError.classList.add('alert','alert-danger','mt-3', 'text-center')
+		let form = document.querySelector('#login');
+		form.appendChild(dataError);
+		setTimeout(function(){
+			form.removeChild(dataError);
+		}, 5000)
+	}
+}
